76ede0b9c7adcaf80caddef1f4fcffef94945cf6
aninstan <aninstan@outlook.com>
2024-10-07 17:52:24 +0200

    added game indicator

---
+++ Python25/flappybird/game.py
@@ -0,0 +1,25 @@
+import pygame
+from settings import WIDTH, HEIGHT
+
+pygame.font.init()
+
+class GameIndicator:
+    def __init__(self, screen):
+        self.screen = screen
+        self.font = pygame.font.SysFont('Bauhaus 93', 60)
+        self.inst_font = pygame.font.SysFont('Bauhaus 93', 30)
+        self.color = pygame.color("white")
+        self.inst_color = pygame.color("black")
+
+    def show_score(self, int_score):
+        bird_score = str(int_score)
+        score = self.font.render(bird_score, True, self.color)
+        self.screen.blit(score, (WIDTH // 2, 50))
+
+    def instructions(self):
+        inst_text1 = "Press SPACE to Jump,"
+        inst_text2 = "Press \"R\" to restart game."
+        inst1 = self.inst_font.render(inst_text1, True, self.inst_color)
+        inst2 = self.inst_font.render(inst_text2, True, self.inst_color)
+        self.screen.blit(inst1, (95,400))
+        self.screen.blit(inst2, (70,450))
